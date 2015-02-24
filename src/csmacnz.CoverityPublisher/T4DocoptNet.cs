dc9a2f36689b88fc5e636fc6fca50e1a9cd165c5
Mark Clearwater <csmacnz@csmac.co.nz>
2015-02-25 07:57:16 +1300

    updated Main.usage.txt

--- src/csmacnz.CoverityPublisher/T4DocoptNet.cs
+++ src/csmacnz.CoverityPublisher/T4DocoptNet.cs
@@ -8,45 +8,22 @@ namespace csmacnz.CoverityPublisher
     // Generated class for Main.usage.txt
     public class MainArgs
     {
-        public const string USAGE = @"Example usage for T4 Docopt.NET
+        public const string USAGE = @"PublishCoverity - a simple command-line publishing tool for coverity's static analysis results.
 
 Usage:
-  prog command ARG <myarg> [OPTIONALARG] [-o -s=<arg> --long=ARG --switch]
-  prog files FILE...
+  PublishCoverity 
+  PublishCoverity --version
+  PublishCoverity --help
+
 
 Options:
- -o           Short switch.
- -s=<arg>     Short option with arg.
- --long=ARG   Long option with arg.
- --switch     Long switch.
+ -h, --help   Show this screen.
+ --version    Show version.
 
-Explanation:
- This is an example usage file that needs to be customized.
- Every time you change this file, run the Custom Tool command
- on T4DocoptNet.tt to re-generate the MainArgs class
- (defined in T4DocoptNet.cs).
- You can then use the MainArgs classed as follows:
-
-    class Program
-    {
-
-       static void DoStuff(string arg, bool flagO, string longValue)
-       {
-         // ...
-       }
-
-        static void Main(string[] argv)
-        {
-            // Automatically exit(1) if invalid arguments
-            var args = new MainArgs(argv, exit: true);
-            if (args.CmdCommand)
-            {
-                Console.WriteLine(""First command"");
-                DoStuff(args.ArgArg, args.OptO, args.OptLong);
-            }
-        }
-    }
-
+What its for:
+ Reads your coverity static anaysis results as a zip, and submits it to
+ coverity's service. This can be used by your build scripts or with a
+ CI builder server.
 ";
         private readonly IDictionary<string, ValueObject> _args;
         public MainArgs(ICollection<string> argv, bool help = true,
@@ -60,16 +37,8 @@ Explanation:
             get { return _args; }
         }
 
-		public bool CmdCommand { get { return _args["command"].IsTrue; } }
-		public string ArgArg { get { return _args["ARG"].ToString(); } }
-		public string ArgMyarg  { get { return _args["<myarg>"].ToString(); } }
-		public string ArgOptionalarg { get { return _args["OPTIONALARG"].ToString(); } }
-		public bool OptO { get { return _args["-o"].IsTrue; } }
-		public string OptS { get { return _args["-s"].ToString(); } }
-		public string OptLong { get { return _args["--long"].ToString(); } }
-		public bool OptSwitch { get { return _args["--switch"].IsTrue; } }
-		public bool CmdFiles { get { return _args["files"].IsTrue; } }
-		public ArrayList ArgFile { get { return _args["FILE"].AsList; } }
+		public bool OptVersion { get { return _args["--version"].IsTrue; } }
+		public bool OptHelp { get { return _args["--help"].IsTrue; } }
 	
     }
 

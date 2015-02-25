dee4168e7fb4e7329bc1d353ee2ca88f09698c95
Mark Clearwater <mark.clearwater@xero.com>
2015-02-26 09:26:48 +1300

    fixed failing tests.

---
+++ src/csmacnz.CoverityPublisher.Integration.Tests/PublishTests.cs
@@ -0,0 +1,26 @@
+﻿using csmacnz.CoverityPublisher.Integration.Tests.TestFramework;
+using Xunit;
+
+namespace csmacnz.CoverityPublisher.Integration.Tests
+{
+    public class PublishTests
+    {
+        [Fact]
+        public void FileDoesntExist()
+        {
+            var results = ExeTestRunner.RunExe("publish -z doesntExist.zip -r USER/REPO -t FAKE_TOKEN -e a@b.com");
+
+            Assert.NotEqual(0, results.ExitCode);
+            Assert.Contains("Input file 'doesntExist.zip' cannot be found", results.StandardError);
+        }
+
+        [Fact]
+        public void FileInQuotesDoesntExist()
+        {
+            var results = ExeTestRunner.RunExe("publish -z \"doesntExist.zip\" -r USER/REPO -t FAKE_TOKEN -e a@b.com");
+
+            Assert.NotEqual(0, results.ExitCode);
+            Assert.Contains("Input file 'doesntExist.zip' cannot be found", results.StandardError);
+        }
+    }
+}

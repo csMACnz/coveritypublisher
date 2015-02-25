a75a3f6e13a0c4b27e032c058efe16c8b1db3d87
Mark Clearwater <csmacnz@csmac.co.nz>
2015-02-25 23:25:42 +1300

    removed wrong lib folder from nupkg creation

--- build.tasks.ps1
+++ build.tasks.ps1
@@ -173,8 +173,7 @@ task pack-only {
     mkdir $nuget_pack_dir
     cp "$nuspec_filename" "$nuget_pack_dir"
 
-    mkdir "$nuget_pack_dir\lib"
-    cp "$build_output_dir\PublishCoverity.exe" "$nuget_pack_dir\lib"
+    cp "$build_output_dir\PublishCoverity.exe" "$nuget_pack_dir"
 
     $Spec = [xml](get-content "$nuget_pack_dir\$nuspec_filename")
     $Spec.package.metadata.version = ([string]$Spec.package.metadata.version).Replace("{Version}", $script:nugetVersion)

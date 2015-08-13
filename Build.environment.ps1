db51f664fed00d94eb6b8432ac9eae6cb7ed3419
Mark Clearwater <csmacnz@csmac.co.nz>
2015-08-14 07:55:10 +1200

    removed pscx dependency
    *added caching as well

--- Build.environment.ps1
+++ Build.environment.ps1
@@ -1,18 +1,3 @@
 choco install -y psake
-choco install -y pscx
 choco install -y GitVersion.Portable
 choco install -y nuget.CommandLine
-
-$pscxPath = "C:\Program Files (x86)\PowerShell Community Extensions\Pscx3\Pscx";
-
-if (-not (Test-Path $pscxPath))
-{
-    $pscxPath = $null;
-    Write-Host "Searching for the pscx powershell module.";
-    $pscxPath = (Get-ChildItem -Path "C:\Program Files\" -Filter "pscx.dll" -Recurse -ErrorAction SilentlyContinue).FullName;
-    if (!$pscxPath) { $pscxPath = (Get-ChildItem -Path "C:\Program Files (x86)\" -Filter "pscx.dll" -Recurse -ErrorAction SilentlyContinue).FullName; }
-    $pscxPath = Split-Path $pscxPath;
-    Write-Host "Found it at " + $pscxPath;
-}
-
-$env:PSModulePath = $env:PSModulePath + ";" + $pscxPath;
